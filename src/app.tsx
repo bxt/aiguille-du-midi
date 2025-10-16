8c6f2fc6b7d7c63175ce5f930923c8c5f57f1bb2
Bernhard Häussner <bernhard.haeussner@xing.com>
2025-10-16 23:47:33 +0200

    feat: add a basic app layout

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,33 @@
+import { content, header } from "./app.module.css";
 import { Router } from "./router.tsx";
 
+function toggleFullScreen() {
+  if (!document.fullscreenElement) {
+    document.documentElement.requestFullscreen();
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    }
+  }
+}
+
 export function App() {
-  return <Router />;
+  return (
+    <>
+      <div class={header}>
+        <div>
+          <a href="#home">Aiguille du MIDI</a>
+        </div>
+        <div>
+          <a href="https://github.com/bxt/aiguille-du-midi">GitHub</a>
+          <button onClick={toggleFullScreen} type="button">
+            Fullscreen
+          </button>
+        </div>
+      </div>
+      <div class={content}>
+        <Router />
+      </div>
+    </>
+  );
 }
